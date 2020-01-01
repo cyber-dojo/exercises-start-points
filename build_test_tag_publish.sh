1951760d90b861f577483dd30ab736ecf296f1a1
Jon Jagger <jon@jaggersoft.com>
2020-01-01 17:17:02 +0000

    Refactoring; tidy .sh scripts

--- build_test_tag_publish.sh
+++ build_test_tag_publish.sh
@@ -1,12 +1,20 @@
-#!/bin/bash
-set -e
+#!/bin/bash -Ee
 
 readonly TMP_DIR=$(mktemp -d /tmp/cyber-dojo.exercises-start-points.XXXXXXXXX)
 trap "rm -rf ${TMP_DIR} > /dev/null" INT EXIT
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-readonly IMAGE=cyberdojo/exercises-start-points
-readonly SHA=$(cd "${ROOT_DIR}" && git rev-parse HEAD)
-readonly TAG="${SHA:0:7}"
+
+# - - - - - - - - - - - - - - - - - - - - - - - -
+versioner_env_vars()
+{
+  docker run --rm cyberdojo/versioner:latest sh -c 'cat /app/.env'
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - -
+git_commit_sha()
+{
+  echo "$(cd "${ROOT_DIR}" && git rev-parse HEAD)"
+}
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
 build_the_image()
@@ -16,9 +24,34 @@ build_the_image()
     curl_script
     chmod 700 $(script_path)
   fi
-  export GIT_COMMIT_SHA="${SHA}"
-  $(script_path) start-point create "${IMAGE}" --exercises "${ROOT_DIR}"
+  export GIT_COMMIT_SHA="$(git_commit_sha)"
+  $(script_path) start-point create "$(image_name)" --exercises "${ROOT_DIR}"
   unset GIT_COMMIT_SHA
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - -
+assert_equal()
+{
+  local -r expected="${1}"
+  local -r actual="${2}"
+  if [ "${expected}" != "${actual}" ]; then
+    echo ERROR
+    echo "expected:'${expected}'"
+    echo "  actual:'${actual}'"
+    exit 42
+  fi
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - -
+image_name()
+{
+  echo "${CYBER_DOJO_EXERCISES_START_POINTS_IMAGE}"
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - -
+image_sha()
+{
+  docker run --rm "$(image_name)" sh -c 'echo ${SHA}'
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
@@ -50,9 +83,11 @@ script_name()
 # - - - - - - - - - - - - - - - - - - - - - - - -
 tag_the_image()
 {
-  docker tag "${IMAGE}:latest" "${IMAGE}:${TAG}"
-  echo "${SHA}"
-  echo "${TAG}"
+  local -r sha="$(image_sha)"
+  local -r tag="${sha:0:7}"
+  docker tag "$(image_name):latest" "$(image_name):${tag}"
+  echo "${sha}"
+  echo "${tag}"
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
@@ -69,14 +104,18 @@ on_ci_publish_tagged_images()
     return
   fi
   echo 'on CI so publishing tagged images'
+  local -r sha="$(image_sha)"
+  local -r tag="${sha:0:7}"
   # DOCKER_USER, DOCKER_PASS are in ci context
   echo "${DOCKER_PASS}" | docker login --username "${DOCKER_USER}" --password-stdin
-  docker push "${IMAGE}:latest"
-  docker push "${IMAGE}:${TAG}"
+  docker push "$(image_name):latest"
+  docker push "$(image_name):${tag}"
   docker logout
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
+export $(versioner_env_vars)
 build_the_image
+assert_equal "$(git_commit_sha)" "$(image_sha)"
 tag_the_image
 on_ci_publish_tagged_images
