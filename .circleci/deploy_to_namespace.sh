c3468a50ccfa38be060be43ef20f551accac67d3
Jon Jagger <jon@jaggersoft.com>
2020-06-11 19:16:23 +0100

    Provide :- default to optional helm_update argument

--- .circleci/deploy_to_namespace.sh
+++ .circleci/deploy_to_namespace.sh
@@ -31,11 +31,8 @@ readonly NAMESPACE="${1}" # beta | prod
 readonly CYBER_DOJO_EXERCISES_START_POINTS_TAG="${CIRCLE_SHA1:0:7}"
 
 helm_upgrade \
-   "${NAMESPACE}" \
+   "${NAMESPACE}" "exercises-start-points" "praqma/cyber-dojo-service --version 0.2.5" \
    "${CYBER_DOJO_EXERCISES_START_POINTS_IMAGE}" \
    "${CYBER_DOJO_EXERCISES_START_POINTS_TAG}" \
    "${CYBER_DOJO_EXERCISES_START_POINTS_PORT}" \
-   ".circleci/k8s-general-values.yml" \
-   ".circleci/k8s-specific-values.yml" \
-   "exercises-start-points" \
-   "praqma/cyber-dojo-service --version 0.2.5"
+   ".circleci/k8s-general-values.yml"
