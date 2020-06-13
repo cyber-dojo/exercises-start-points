57dc906657dbd48ccd01807401cb47603b197d53
Jon Jagger <jon@jaggersoft.com>
2020-06-13 12:11:59 +0100

    Use deployment functions curled from k8s-install repo

--- .circleci/deploy_to_namespace.sh
+++ .circleci/deploy_to_namespace.sh
@@ -1,37 +1,17 @@
 #!/bin/bash -Eeu
 
-source ./.circleci/helm_upgrade.sh
-
-# misc env-vars are in ci context
-
-echo ${GCP_K8S_CREDENTIALS} > /gcp/gcp-credentials.json
-
-gcloud auth activate-service-account \
-  "${SERVICE_ACCOUNT}" \
-  --key-file=/gcp/gcp-credentials.json
-
-gcloud container clusters get-credentials \
-  "${CLUSTER}" \
-  --zone "${ZONE}" \
-  --project "${PROJECT}"
-
-helm init --client-only
-
-helm repo add praqma https://praqma-helm-repo.s3.amazonaws.com/
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Normally I export the cyberdojo env-vars using the command
-# $ docker run --rm cyberdojo/versioner:latest
-# This won't work on the.circleci deployment step since it is
-# run inside the cyberdojo/gcloud-kubectl-helm image which does
-# not have docker. So doing it directly from versioner's git repo
-export $(curl https://raw.githubusercontent.com/cyber-dojo/versioner/master/app/.env)
-
-readonly NAMESPACE="${1}" # beta | prod
+readonly NAMESPACE="${1}" # beta|prod
+readonly K8S_URL=https://raw.githubusercontent.com/cyber-dojo/k8s-install/master
+readonly VERSIONER_URL=https://raw.githubusercontent.com/cyber-dojo/versioner/master
+source <(curl "${K8S_URL}/sh/deployment_functions.sh")
+export $(curl "${VERSIONER_URL}/app/.env")
 readonly CYBER_DOJO_EXERCISES_START_POINTS_TAG="${CIRCLE_SHA1:0:7}"
 
-helm_upgrade \
-   "${NAMESPACE}" "exercises-start-points" "praqma/cyber-dojo-service --version 0.2.5" \
+gcloud_init
+helm_init
+helm_upgrade_probe_yes_prometheus_yes \
+   "${NAMESPACE}" \
+   "exercises-start-points" \
    "${CYBER_DOJO_EXERCISES_START_POINTS_IMAGE}" \
    "${CYBER_DOJO_EXERCISES_START_POINTS_TAG}" \
    "${CYBER_DOJO_EXERCISES_START_POINTS_PORT}" \
