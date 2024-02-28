8a82ea019afdcdb9631b9b9bd246fc4d090ece21
JonJagger <jon@kosli.com>
2024-02-28 14:04:07 +0000

    Rename snyk policy file back to .snyk
        Despite what the docs say, you cannot use a policy file
        with any other name than .snyk
        If you do, it is ignored with no warnings.
        Sheesh.

--- sh/kosli.sh
+++ sh/kosli.sh
@@ -65,7 +65,7 @@ kosli_attest_snyk()
     --artifact-type=docker \
     --host="${hostname}" \
     --api-token="${api_token}" \
-    --attachments="$(repo_root)/snyk.policy" \
+    --attachments="$(repo_root)/.snyk" \
     --name=exercises-start-points.snyk-scan \
     --scan-results="$(repo_root)/snyk.json"
 }
@@ -119,7 +119,7 @@ on_ci_kosli_attest_snyk_scan_evidence()
       --file=Dockerfile \
       --sarif \
       --sarif-file-output=snyk.json \
-      --policy-path="$(repo_root)/snyk.policy"
+      --policy-path="$(repo_root)/.snyk"
     set -e
 
     kosli_attest_snyk "${KOSLI_HOST_STAGING}"    "${KOSLI_API_TOKEN_STAGING}"
